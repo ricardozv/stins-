1572b61ab972cbd7e617a186c5a1cf830f10654d
ricozv <ricardosilvanet03@gmail.com>
2020-01-22 23:09:10 -0300

    upload server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.send(' Servidor rodando na bagaceira!')
+    return res.send({ message : "Fala sério!"})
 });
 
 app.listen(3333);
