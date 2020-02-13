5e59f47b50a883f57bc547b20e9752ae39a18ecc
ricardozv <ricardosilvanet03@gmail.com>
2020-02-13 18:19:05 -0500

    upload

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,5 +1,23 @@
-// index, show, store, update, destroy 
 
 module.exports = {
-    
-}
+    store(req, res) {
+        return res.json({ mesage: 'Fala sério!'});
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+// index: método que retorna uma listagem de sessões
+// show: método que lista uma única sessão
+// store: método que cria um sessão
+// update: método que altera uma sessão
+// destroy: método que remove uma sessão
