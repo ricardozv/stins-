0244d0306be259e0119f39102498148dd743dfab
ricardozv <ricardosilvanet03@gmail.com>
2020-02-27 08:56:56 -0500

    up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 
 import logo from './assets/logo.svg';
 
+import Routes from './routes';
 
 function App() {
   return (
     <div className = "container">
       <img src={logo} alt="logo"/>
+      <div className="content">
+        <Routes />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
